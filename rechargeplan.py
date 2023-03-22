fe65c279308b4c4d3371a5a699f2d909b485e170
Robinkumar J <128570607+RobinkumarJ246@users.noreply.github.com>
2023-03-22 22:22:56 +0530

    Added comments

--- rechargeplan.py
+++ rechargeplan.py
@@ -1,8 +1,14 @@
+#github.com/RobinKumarJ246/Python
+
 import json
 import time
 import datetime
 from datetime import date
 from datetime import datetime, timedelta
+
+#all datas are stored and retreived using plan.json
+#set expiry date with respect to today's date(date of activation)
+#time.sleep() just to bring some real effect
 
 today = datetime.today()
 new_date = today + timedelta(days=24)
@@ -11,6 +17,8 @@ expire_date = new_date.strftime('%d-%m-%Y')
 
 print("Welcome to the portal")
 time.sleep(1)
+
+#ask user for choice
 choice = int(input("Type an input\n1 for activate a plan\n2 to Deactivate a plan\n3 to Check plan details\nInput : "))
 if choice == 1:
     with open('plan.json','r') as f:
@@ -19,7 +27,7 @@ if choice == 1:
         amt = int(input("Enter an amount to recharge : "))
         while amt<=298 or amt >=2000:
             amt = int(input("Enter an amount between 299 and 2000 : "))
-        
+        #set the values to be changed in plan.json
         x["plan"] = "active"
         x["price"] = amt
         x["expire"] = expire_date
